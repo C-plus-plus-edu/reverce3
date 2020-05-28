bcb22ae038eaeb30926e9af26abf87cd0dca60c1
Andrey Balamut <balamut.andrey@gmail.com>
2020-05-29 01:06:07 +0300

    first commit

---
+++ src/reverce3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> Reversed(const vector<int>& v) {
+	vector<int> temp;
+
+	for (int i = v.size() - 1; i >= 0; --i) {
+		temp.push_back(v[i]);
+	}
+	return temp;
+}
+
+int main() {
+	vector<int> numbers = {1, 5, 3, 4, 2};
+	numbers = Reversed(numbers);
+	for (auto i : numbers) {
+		cout << i << " ";
+	}
+	return 0;
+}
